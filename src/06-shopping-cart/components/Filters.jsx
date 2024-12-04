a96b73a9f1a9759dd49f7d4c4a378421634de1ab
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-04 16:53:01 -0300

    Create Filters.jsx

---
+++ src/06-shopping-cart/components/Filters.jsx
@@ -0,0 +1,57 @@
+import { useId } from "react";
+import { useFilters } from "../hooks/useFilters.js";
+import "./Filters.css";
+
+export function Filters() {
+  const { filters, setFilters } = useFilters();
+
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
+
+  const handleChangeMinPrice = (event) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    // ⬇️ ESTO HUELE MAL
+    // estamos pasando la función de actualizar estado
+    // nativa de React a un componente hijo
+    setFilters((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
+
+  return (
+    <section className="filters">
+      <div className="pricefilter">
+        <label htmlFor={minPriceFilterId}>Precio a partir de:</label>
+        <input
+          type="range"
+          id={minPriceFilterId}
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+          value={filters.minPrice}
+        />
+        <span>${filters.minPrice}</span>
+      </div>
+
+      <div className="categoryfilter">
+        <label htmlFor={categoryFilterId}>Categoría</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">Todas</option>
+          <option value="laptops">Portátiles</option>
+          <option value="smartphones">Celulares</option>
+          <option value="groceries">comestibles</option>
+          <option value="home-decoration">decoracion casa</option>
+          <option value="skincare">cuidado de piel</option>
+          <option value="fragrances">fragancias</option>
+        </select>
+      </div>
+    </section>
+  );
+}
