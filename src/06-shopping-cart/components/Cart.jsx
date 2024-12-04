45bda8abfa96f29c208096c16f39f9e05496fa17
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-04 16:54:22 -0300

    Create Cart.jsx

---
+++ src/06-shopping-cart/components/Cart.jsx
@@ -0,0 +1,70 @@
+import "./Cart.css";
+import { productos as initialProducts } from "../mocks/products.json";
+import { ClearCartIcon } from "./Icons.jsx";
+import Home from "../assets/home-icon-transparent-free.png";
+import { useCart } from "../hooks/useCart.js";
+import { CartItem } from "./CartItem.jsx";
+import CartPage from "../pages/CartPage.jsx";
+import { useNavigate } from "react-router-dom";
+
+export function Cart() {
+  const MINIM_AMOUNT = 7000;
+  const navigte = useNavigate();
+
+  const { cart, clearCart, addToCart, decrementProduct } = useCart();
+
+  let amount = cart
+    .map((x) => {
+      let { quantity, id } = x;
+      let search = initialProducts.find((y) => y.id === id) || [];
+
+      return quantity * search.price;
+    })
+    .reduce((x, y) => x + y, 0);
+
+  return (
+    <>
+      <div className="nav">
+        <div className="padrehome">
+          <div id="home">
+            <img
+              src={Home}
+              width="105px"
+              height="95px"
+              onClick={() => navigte("/")}
+            ></img>
+            <h2>Back to home</h2>
+          </div>
+        </div>
+        <CartPage />
+      </div>
+      <section className="container">
+        <ul className="cart">
+          {cart.map((product) => (
+            <CartItem
+              {...product}
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              decrementProduct={() => decrementProduct(product)}
+            />
+          ))}
+        </ul>
+        <div className="cartfooter">
+          <h3>{`Total: $${amount}`}</h3>
+          <button className={"clearcart"} onClick={clearCart}>
+            <ClearCartIcon />
+          </button>
+          <button
+            className={`${
+              amount === MINIM_AMOUNT || amount > MINIM_AMOUNT
+                ? "finalizebuy good"
+                : "finalizebuy"
+            }`}
+          >
+            Comprar
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
