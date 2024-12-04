65e3e0963bded2563f85398271d3c864b0603624
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-04 15:41:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./06-shopping-cart/App";
+import { FiltersProvider } from "./06-shopping-cart/context/filters";
+import "./06-shopping-cart/index.css";
+createRoot(document.getElementById("root")).render(
+  <FiltersProvider>
+    <App />
+  </FiltersProvider>
+);
 
