1e0c0ecee3f714a01e811e5d46302b4150175f03
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-09 21:58:04 -0300

    Create WishListItem.jsx

---
+++ src/06-shopping-cart/components/WishListItem.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./WishListItem.css";
+import { AddToCartIcon, RemoveFromCartIcon } from "./Icons.jsx";
+import { RiHeart3Fill } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+
+export function WishListItem({
+  id,
+  thumbnail,
+  price,
+  title,
+  wishlistitems,
+  isProductInCart,
+  isProductInWishList,
+  addToCart,
+  removeFromCart,
+  addToWishList,
+  removeFromWishList,
+}) {
+  return (
+    <li key={id}>
+      <div className="content">
+        <img src={thumbnail} alt={title} />
+        <strong id="title">
+          {title.length > 14 ? `${title.slice(0, 14)}...` : `${title}`}
+        </strong>
+        <strong id="price">{`$${price}`}</strong>
+        <RiHeart3Fill
+          className="heart"
+          style={{
+            color: "red",
+          }}
+          onClick={() => {
+            isProductInWishList
+              ? removeFromWishList(wishlistitems)
+              : addToWishList(wishlistitems);
+          }}
+        />
+      </div>
+
+      <NavLink to={`/products/${id}`}>Ver producto</NavLink>
+      <div className="isproductincart">
+        <button
+          style={{
+            backgroundColor: isProductInCart ? "red" : "#09f",
+          }}
+          onClick={() => {
+            isProductInCart ? removeFromCart(product) : addToCart(product);
+          }}
+        >
+          {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+        </button>
+      </div>
+    </li>
+  );
+}
