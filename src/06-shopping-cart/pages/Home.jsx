94fe0e9ccc3309e36c8689badb352a16e377c28a
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-09 22:02:04 -0300

    Update Home.jsx

--- src/06-shopping-cart/pages/Home.jsx
+++ src/06-shopping-cart/pages/Home.jsx
@@ -1,22 +1,22 @@
-import { Header } from "../components/Header";
 import { Products } from "../components/Products";
 import { Filters } from "../components/Filters.jsx";
 import { useFilters } from "../hooks/useFilters.js";
 import { productos as initialProducts } from "../mocks/products.json";
-import CartPage from "../pages/CartPage.jsx";
+import { Navbar } from "../components/Navbar.jsx";
+import "./Home.css";
 //import { IS_DEVELOPMENT } from "../config.js";
-
 export function Home() {
   const { filterProducts } = useFilters();
-
   const filteredProducts = filterProducts(initialProducts);
 
   return (
-    <div>
-      <CartPage />
-      <Header />
-      <Filters />
-      <Products products={filteredProducts} />
-    </div>
+    <>
+      <Navbar />
+      <br />
+      <main>
+        <Filters />
+        <Products products={filteredProducts} />
+      </main>
+    </>
   );
 }
