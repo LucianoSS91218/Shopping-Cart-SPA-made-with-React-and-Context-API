04df6b0469c5945b8238c30760849a9fdbed8335
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-09 22:00:01 -0300

    Update App.jsx

--- src/06-shopping-cart/App.jsx
+++ src/06-shopping-cart/App.jsx
@@ -3,18 +3,23 @@ import { Home } from "./pages/Home.jsx";
 import { ErrorPage } from "./pages/NotFound.jsx";
 import { ProductDetail } from "./pages/ProductDetail.jsx";
 import { Cart } from "./components/Cart.jsx";
+import { WishList } from "./components/WishList.jsx";
+import { WishListProvider } from "./context/wishlist.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <CartProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="products/:id" element={<ProductDetail />} />
-          <Route path="/:rest/*" element={<ErrorPage />} />
-        </Routes>
+        <WishListProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="products/:id" element={<ProductDetail />} />
+            <Route path="wishlist" element={<WishList />} />
+            <Route path="/:rest/*" element={<ErrorPage />} />
+          </Routes>
+        </WishListProvider>
       </CartProvider>
     </Router>
   );
