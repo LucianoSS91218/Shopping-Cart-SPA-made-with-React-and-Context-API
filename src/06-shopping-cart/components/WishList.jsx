0521c809b82c1155516016ee416ee7ab87ce5609
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-09 21:58:25 -0300

    Create WishList.jsx

---
+++ src/06-shopping-cart/components/WishList.jsx
@@ -0,0 +1,65 @@
+import "./WishList.css";
+import { useWishList } from "../hooks/useWishList.js";
+import { WishListItem } from "./WishListItem.jsx";
+import { useCart } from "../hooks/useCart.js";
+import { Navbar } from "../components/Navbar.jsx";
+import { useState } from "react";
+export function WishList() {
+  const { addToCart, removeFromCart, cart } = useCart();
+  const checkProductInCart = (product) => {
+    return cart.some((item) => item.id === product.id);
+  };
+
+  const { addToWishList, removeFromWishList, clearWishList, wishlist } =
+    useWishList();
+
+  const checkProductInWishList = (product) => {
+    return wishlist.some((item) => item.id === product.id);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <section className="container">
+        <h3>{wishlist?.length + " favoritos"}</h3>
+        {wishlist?.length ? (
+          <ul className="wishlist">
+            {wishlist.map((wishlistitems) => {
+              const isProductInCart = checkProductInCart(wishlistitems);
+              const isProductInWishList = checkProductInWishList(wishlistitems);
+              return (
+                <WishListItem
+                  key={wishlistitems.id}
+                  id={wishlistitems.id}
+                  thumbnail={wishlistitems.thumbnail}
+                  price={wishlistitems.price}
+                  title={wishlistitems.title}
+                  wishlistitem={wishlistitems}
+                  isProductInCart={isProductInCart}
+                  isProductInWishList={isProductInWishList}
+                  addToCart={addToCart}
+                  removeFromCart={removeFromCart}
+                  addToWishList={addToWishList}
+                  removeFromWishList={removeFromWishList}
+                />
+              );
+            })}
+          </ul>
+        ) : (
+          <h2>No tenes productos en lista de favoritos</h2>
+        )}
+        <div className="wlfooter">
+          <button className={"clearwishlist"} onClick={clearWishList}>
+            Borrar toda la lista de favoritos
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
