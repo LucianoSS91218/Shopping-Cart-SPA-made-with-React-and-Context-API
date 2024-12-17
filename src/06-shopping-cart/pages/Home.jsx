b0b7d6e2034adc838a7067de57f7c12011b8e4bd
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-16 22:23:00 -0300

    Update Home.jsx

--- src/06-shopping-cart/pages/Home.jsx
+++ src/06-shopping-cart/pages/Home.jsx
@@ -4,14 +4,23 @@ import { useFilters } from "../hooks/useFilters.js";
 import { productos as initialProducts } from "../mocks/products.json";
 import { Navbar } from "../components/Navbar.jsx";
 import "./Home.css";
+import { useRef } from "react";
+import useDarkLight from "../dark-light/hooks/useDarkLight.js";
+import "../dark-light/DarkLight.css";
 //import { IS_DEVELOPMENT } from "../config.js";
 export function Home() {
   const { filterProducts } = useFilters();
   const filteredProducts = filterProducts(initialProducts);
 
+  const [theme, setTheme] = useDarkLight("theme", "light");
+  function handleToggleTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
   return (
     <>
-<Navbar />
+      <div className="light-dark-mode" data-theme={theme}>
+  <Navbar />
       <br />
       <header>
         <Filters />
@@ -19,7 +28,14 @@ export function Home() {
       <main>
         <Products products={filteredProducts} />
       </main>
+        <br />
+        <button id="changedm" onClick={handleToggleTheme}>
+          Change Theme
+        </button>
+        <br />
+        <br />
       <footer>Copyright Luciano Sanuni</footer>
+          </div>
     </>
   );
 }
