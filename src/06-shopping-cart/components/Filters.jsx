82718ea1398b8266b43e9607260088a4f2a06f3c
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-22 12:19:59 -0300

    Update Filters.jsx

--- src/06-shopping-cart/components/Filters.jsx
+++ src/06-shopping-cart/components/Filters.jsx
@@ -5,7 +5,77 @@ import "./Filters.css";
 export function Filters() {
   const { filters, setFilters } = useFilters();
 
+  const minPriceFilterId = useId();import { useId } from "react";
+import { useFilters } from "../hooks/useFilters.js";
+import "./Filters.css";
+
+export function Filters({ isDarkMode }) {
+  const { filters, setFilters } = useFilters();
+
   const minPriceFilterId = useId();
+  //const categoryFilterId = useId();
+
+  const handleChangeMinPrice = (event) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (text) => {
+    setFilters((prevState) => ({
+      ...prevState,
+      category: text,
+    }));
+  };
+
+  const objcategories = {
+    all: "Todos",
+    "white spirits": "Bebidas blancas",
+    smartphones: "Celulares",
+    laptops: "Laptops",
+    groceries: "Comestibles",
+    "home-decoration": "Decoracion para casa",
+    skincare: "Cuidado de piel",
+    fragrances: "Fragancias",
+  };
+  
+  return (
+    <>
+      <section
+        className={isDarkMode === "light" ? "filters" : "filtersdarkmode"}
+      >
+        <p id="namefilter">Filtros</p>
+        <p id="namecategory">Categoria</p>
+        <ul>
+          {Object.entries(objcategories).map(([key, literal]) => (
+            <li
+              key={key}
+              className={key === filters.category ? "activecategoryfilter" : ""}
+              onClick={() => handleChangeCategory(key)}
+            >
+              {literal}
+            </li>
+          ))}
+        </ul>
+        <div className="pricefilter">
+          <label htmlFor={minPriceFilterId}>Rangos de precio:</label>
+          <span id="minprice">${filters.minPrice}</span>
+          <input
+            type="range"
+            id={minPriceFilterId}
+            min="0"
+            max="2000"
+            onChange={handleChangeMinPrice}
+            value={filters.minPrice}
+          />
+          <span>$400 - 2000</span>
+        </div>
+      </section>
+    </>
+  );
+}
+
   const categoryFilterId = useId();
 
   const handleChangeMinPrice = (event) => {
