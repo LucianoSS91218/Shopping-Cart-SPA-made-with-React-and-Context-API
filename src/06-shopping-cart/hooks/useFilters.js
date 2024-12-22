471b636a685897f29c25e79620c536e13fa3218a
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-22 17:01:37 -0300

    Update useFilters.js

--- src/06-shopping-cart/hooks/useFilters.js
+++ src/06-shopping-cart/hooks/useFilters.js
@@ -5,13 +5,24 @@ export function useFilters() {
   const { filters, setFilters } = useContext(FiltersContext);
 
   const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
+  const filteredProducts = products.filter((product) => {
+    return (
+      product.price >= filters.minPrice &&
+      (filters.category === "all" || product.category === filters.category)
+    );
+  });
+
+  const sortStrategies = {
+    minprice: (a, b) => a.price - b.price,
+    maxprice: (a, b) => b.price - a.price,
+    nameasc: (a, b) => a.title.localeCompare(b.title)),
+    namedesc: (a, b) => b.title.localeCompare(a.title)),
   };
+
+  const sortFunction = sortStrategies[filters.sorttype] || (() => 0); // Default: no sorting
+  return filteredProducts.sort(sortFunction);
+};
+
 
   return { filters, filterProducts, setFilters };
 }
