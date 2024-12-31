2b866dd07effd5ff6180f7cd3e41276c380560e3
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2024-12-31 19:33:57 -0300

    Update Home.jsx

--- src/06-shopping-cart/pages/Home.jsx
+++ src/06-shopping-cart/pages/Home.jsx
@@ -4,37 +4,14 @@ import { productos as initialProducts } from "../mocks/products.json";
 import { Navbar } from "../components/Navbar.jsx";
 import "./Home.css";
 import { useRef } from "react";
-import useDarkLight from "../dark-light/hooks/useDarkLight.js";
-import "../dark-light/DarkLight.css";
-import useNearScreen from "../hooks/useNearScreen.js";
 
 export function Home() {
   const { filters, filterProducts } = useFilters();
   const filteredProducts = filterProducts(initialProducts);
 
-  const [theme, setTheme] = useDarkLight("theme", "light");
-
-  const externalRef = useRef();
-  const { isNearScreen } = useNearScreen({
-    distance: "3100px",
-    externalRef,
-  });
-
-  function handleToggleTheme() {
-    setTheme(theme === "dark" ? "light" : "dark");
-  }
-
   return (
     <>
-      <div className="light-dark-mode" data-theme={theme}>
         <Navbar />
-        <button
-          id="changedm"
-          onClick={handleToggleTheme}
-          className={isNearScreen ? "fixd" : ""}
-        >
-          Change Theme
-        </button>
         <main>
           <div className="box">
             {filteredProducts.length > 0 ? (
@@ -47,10 +24,9 @@ export function Home() {
           </div>
         </main>
         <div ref={externalRef}></div>
-        <footer className={theme === "light" ? "footerwhite" : ""}>
+        <footer>
           Copyright Luciano Sanuni
         </footer>
-      </div>
     </>
   );
 }
