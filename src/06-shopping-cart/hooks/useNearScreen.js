72e0d72f6cd33ec2614559ed57259fe8879f963b
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2025-01-02 22:22:30 -0300

    Update useNearScreen.js

--- src/06-shopping-cart/hooks/useNearScreen.js
+++ src/06-shopping-cart/hooks/useNearScreen.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 
 export default function useNearScreen({
-  distance = "1500px",
+  distance = "300px",
   externalRef,
 } = {}) {
   const [isNearScreen, setIsNearScreen] = useState(false);
