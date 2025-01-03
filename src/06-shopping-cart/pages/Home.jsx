6947c554cbce50b3c985886de7b02a60618b2436
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2025-01-02 22:22:05 -0300

    Update Home.jsx

--- src/06-shopping-cart/pages/Home.jsx
+++ src/06-shopping-cart/pages/Home.jsx
@@ -1,20 +1,48 @@
 import { Products } from "../components/Products";
+import { FiltersDesktop } from "../components/FiltersDesktop.jsx";
 import { useFilters } from "../hooks/useFilters.js";
 import { productos as initialProducts } from "../mocks/products.json";
 import { Navbar } from "../components/Navbar.jsx";
 import "./Home.css";
+import { useRef } from "react";
+import useDarkLight from "../dark-light/hooks/useDarkLight.js";
+import "../dark-light/DarkLight.css";
+import useNearScreen from "../hooks/useNearScreen.js";
 
 export function Home() {
   const { filters, filterProducts } = useFilters();
   const filteredProducts = filterProducts(initialProducts);
 
+  const [theme, setTheme] = useDarkLight("theme", "light");
+
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    distance: "200px",
+    externalRef,
+  });
+
+  function handleToggleTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
   return (
     <>
+      <div className="light-dark-mode" data-theme={theme}>
         <Navbar />
+        <button
+          id="changedm"
+          onClick={handleToggleTheme}
+          className={isNearScreen ? "fixd" : ""}
+        >
+          Change Theme
+        </button>
         <main>
           <div className="box">
+            <FiltersDesktop isDarkMode={theme} />
             {filteredProducts.length > 0 ? (
-              <Products products={filteredProducts} />
+              <>
+                <Products products={filteredProducts} isDarkMode={theme} />
+              </>
             ) : (
               <div className="noproducts">
                 <h3>No hay productos de {filters.category} para mostrar</h3>
@@ -22,9 +50,11 @@ export function Home() {
             )}
           </div>
         </main>
-        <footer>
+        <div ref={externalRef}></div>
+        <footer className={theme === "light" ? "footerwhite" : ""}>
           Copyright Luciano Sanuni
         </footer>
+      </div>
     </>
   );
 }
